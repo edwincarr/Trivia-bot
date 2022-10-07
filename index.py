3fe9b81510d0c28eba3d5a8b05d08c9456555f3c
joshm <josh.marek22@gmail.com>
2022-10-07 19:56:47 -0400

    {added wrong answer and correct answer stuff}

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
 import requests
+import random
+
 
 def triv():
+  rand = random.randint(0, 3)
   r = requests.get('https://the-trivia-api.com/api/questions?limit=1')
   DATA = r.json()[0]
   QUESTION = DATA['question']
+  WRONG = DATA['incorrectAnswers']
   CORRECT_ANSWER = DATA['correctAnswer']
   # ANSWER = 
-
+  print(QUESTION , WRONG , CORRECT_ANSWER) 
 triv()
