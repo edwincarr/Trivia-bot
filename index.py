e2bfb4dd3a605c1946095238146722168fec6eab
edwin carrillo <edwincarrillo9@outlook.com>
2022-10-07 19:42:58 -0400

    parsed data out of JSON

--- index.py
+++ index.py
@@ -2,8 +2,9 @@ import requests
 
 def triv():
   r = requests.get('https://the-trivia-api.com/api/questions?limit=1')
-  data = r.json()
-  print(data[0])
-
+  DATA = r.json()[0]
+  QUESTION = DATA['question']
+  CORRECT_ANSWER = DATA['correctAnswer']
+  # ANSWER = 
 
 triv()
