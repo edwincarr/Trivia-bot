b7ab4bb0df378a92a86597278152e704048e6e63
edwin carrillo <edwincarrillo9@outlook.com>
2022-10-07 20:19:43 -0400

    added input and conditional

--- index.py
+++ index.py
@@ -1,14 +1,23 @@
 import requests
 import random
 
-
 def triv():
   rand = random.randint(0, 3)
   r = requests.get('https://the-trivia-api.com/api/questions?limit=1')
+
   DATA = r.json()[0]
   QUESTION = DATA['question']
-  WRONG = DATA['incorrectAnswers']
   CORRECT_ANSWER = DATA['correctAnswer']
-  # ANSWER = 
-  print(QUESTION , WRONG , CORRECT_ANSWER) 
+  answers = DATA['incorrectAnswers']
+  answers.insert(rand, CORRECT_ANSWER)
+
+  print(QUESTION)
+  print(answers)
+  user_res = int(input('Give Correct Index: '))
+
+  if answers[user_res] == CORRECT_ANSWER:
+    print('Your cooking with gas')
+  else:
+    print(f'{CORRECT_ANSWER}... Try Again')
+
 triv()
